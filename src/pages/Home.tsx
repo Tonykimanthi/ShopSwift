3d1bb37654bdbb490a73a94ff1732cd0c3064d1e
Tony Km <kimaxhanthony9@gmail.com>
2023-12-26 22:22:36 +0300

    Edit: Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,15 +25,15 @@ const Home = () => {
 
 
   return (
-    <main className="bg-primary-lightblue h-96 text-black px-32 flex justify-between items-center overflow-hidden">
+    <main className="bg-primary-lightblue h-96 text-black px-32 flex justify-between items-center overflow-hidden max-lg:px-10 max-sm:flex-col-reverse max-sm:justify-center max-sm:h-fit max-sm:py-6">
       <motion.section key={sectionKey} initial={{opacity: 0, y: 100 + "%"}} animate={{opacity: 1, y: 0}} transition={{duration: .5, delay: .6}}>
         <h2 className="text-6xl font-bold">{homePageList[currentIndex].title}</h2>
         <p className="mt-1 font-medium">{homePageList[currentIndex].description}</p>
-        <button className="mt-5 px-6 py-2 text-white rounded bg-primary-darkblue hover:bg-black transition">
+        <button className="mt-5 px-6 py-2 text-white rounded bg-primary-darkblue hover:bg-black transition max-sm:block max-sm:mx-auto">
           <Link to="shop">Shop now</Link>
         </button>
       </motion.section>
-      <motion.aside key={asideKey} className="w-96" initial={{x: 100 + "%", opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 1}}>
+      <motion.aside key={asideKey} className="w-96 max-sm:w-40" initial={{x: 100 + "%", opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 1}}>
         <img
           className="object-cover"
           src={homePageList[currentIndex].image}
