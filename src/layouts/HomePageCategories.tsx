6144b266476f1eb5d9036e67e684d88f282cb324
Tony Km <kimaxhanthony9@gmail.com>
2023-12-27 19:42:33 +0300

    Edit: HomePageCategories.tsx

--- src/layouts/HomePageCategories.tsx
+++ src/layouts/HomePageCategories.tsx
@@ -18,14 +18,19 @@ const HomePageCategories = () => {
     {
       title: "Men's clothing",
       image: "/images/men-clothing2.jpg",
-      customStyles: "row-start-1 col-start-3 row-span-2 max-sm:h-56 max-sm:row-auto max-sm:col-auto",
+      customStyles:
+        "row-start-1 col-start-3 row-span-2 max-sm:h-56 max-sm:row-auto max-sm:col-auto",
     },
   ];
 
   return (
     <section className="my-5 px-10 grid grid-cols-3 grid-rows-2 gap-2 max-sm:grid-cols-1 max-sm:px-5">
       {categories.map((category, index) => (
-        <article key={index} style={{backgroundImage: `url(${category.image})`}} className={`bg-center relative ${category.customStyles}`}>
+        <article
+          key={index}
+          style={{ backgroundImage: `url(${category.image})` }}
+          className={`bg-center bg-cover relative ${category.customStyles}`}
+        >
           <div className="absolute top-24 left-8 text-white">
             <h4 className="text-3xl font-bold">{category.title}</h4>
             <button className="mt-2 px-4 py-1 font-medium rounded border border-primary-darkblue hover:bg-primary-darkblue transition">
