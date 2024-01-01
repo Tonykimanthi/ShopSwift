97a208a789fa0c0237e6dcb33f839ddaa262a0f2
Tony Km <kimaxhanthony9@gmail.com>
2024-01-01 16:44:24 +0300

    Edit: HomePageCategories.tsx

--- src/layouts/HomePageCategories.tsx
+++ src/layouts/HomePageCategories.tsx
@@ -1,25 +1,31 @@
+import { Link } from "react-router-dom";
+
 const HomePageCategories = () => {
   const categories = [
     {
       title: "Electronics",
       image: "/images/electronics2.jpg",
       customStyles: "",
+      path: "electronics",
     },
     {
       title: "Women's clothing",
       image: "/images/women-clothing2.jpg",
       customStyles: "",
+      path: "womenclothing",
     },
     {
       title: "Jewelleries",
       image: "/images/jewellery2.jpg",
       customStyles: "col-span-2 max-sm:col-span-1",
+      path: "jewelleries",
     },
     {
       title: "Men's clothing",
       image: "/images/men-clothing2.jpg",
       customStyles:
         "row-start-1 col-start-3 row-span-2 max-sm:row-auto max-sm:col-auto",
+      path: "menclothing",
     },
   ];
 
@@ -30,12 +36,18 @@ const HomePageCategories = () => {
           key={index}
           className={`bg-center bg-cover relative cursor-pointer overflow-hidden group ${category.customStyles}`}
         >
-          <img className="w-full h-full object-cover group-hover:scale-110 transition duration-300" src={category.image} alt="" />
+          <img
+            className="w-full h-full object-cover group-hover:scale-110 transition duration-300"
+            src={category.image}
+            alt=""
+          />
           <div className="absolute top-24 left-8 text-white">
             <h4 className="text-3xl font-bold">{category.title}</h4>
-            <button className="mt-2 px-4 py-1 font-medium rounded border border-primary-darkblue hover:bg-primary-darkblue transition">
-              Shop now
-            </button>
+            <Link to={`/${category.path}`}>
+              <button className="mt-2 px-4 py-1 font-medium rounded border border-primary-darkblue hover:bg-primary-darkblue transition">
+                Shop now
+              </button>
+            </Link>
           </div>
         </article>
       ))}
