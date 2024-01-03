474c7e865ad7f96cb5ad65eb39f9c57d8fcaab11
Tony Km <kimaxhanthony9@gmail.com>
2024-01-03 19:28:49 +0300

    Edit: ProductDetails.tsx

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import ErrorMessage from "../components/ErrorMessage";
 import Loader from "../components/Loader";
 import useFetch from "../hooks/useFetch";
+import { Link } from "react-router-dom";
 
 type productProps = {
   id: number;
@@ -9,6 +10,11 @@ type productProps = {
   price: number;
   description: string;
   image: string;
+  category: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
 };
 
 const ProductDetails = () => {
@@ -26,20 +32,45 @@ const ProductDetails = () => {
   }, [data]);
 
   return (
-    <main className="mt-4 flex flex-col items-center">
+    <main className="mt-5 px-10 flex flex-col items-center">
       {loading && <Loader />}
       {!loading && error ? (
         <ErrorMessage />
       ) : (
         <>
           {product && (
-            <article className="flex bg-slate-200">
+            <article className="grid grid-cols-[30%_auto] bg-slate-100">
               <div>
-                <img className="w-96" src={product.image} alt={product.title} />
+                <img
+                  className="object-cover"
+                  src={product.image}
+                  alt={product.title}
+                />
               </div>
-              <div>
-                <h2>{product.title}</h2>
-                <p>{product.description}</p>
+              <div className="px-5 py-2">
+                <h2 className="text-3xl font-medium leading-tight">
+                  {product.title}
+                </h2>
+                <p className="mt-2 text-2xl leading-tight">
+                  {product.description}
+                </p>
+                <div>
+                  <span className="mt-2 block text-lg italic font-medium">
+                    Category:{" "}
+                    <Link
+                      to="/menclothing"
+                      className="not-italic text-primary-blue hover:underline underline-offset-2 capitalize"
+                    >
+                      {product.category}
+                    </Link>
+                  </span>
+                  <span className="text-lg italic font-medium">
+                    Rating:{" "}
+                    <small className="text-lg text-primary-green font-medium">
+                      {product.rating.rate}
+                    </small>
+                  </span>
+                </div>
               </div>
             </article>
           )}
