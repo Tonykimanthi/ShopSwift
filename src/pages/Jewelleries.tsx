8c4512a75203f1caf82ca1a20b5a1aaba26da169
Tony Km <kimaxhanthony9@gmail.com>
2024-01-04 15:03:52 +0300

    Edit: Jewelleries.tsx

--- src/pages/Jewelleries.tsx
+++ src/pages/Jewelleries.tsx
@@ -2,6 +2,7 @@ import useFetch from "../hooks/useFetch";
 import Loader from "../components/Loader";
 import ErrorMessage from "../components/ErrorMessage";
 import AddToCartBtn from "../components/AddToCartBtn";
+import { Link } from "react-router-dom";
 
 interface productProps {
   id: number;
@@ -15,7 +16,6 @@ const Jewelleries = () => {
     "https://fakestoreapi.com/products/category/jewelery"
   );
 
-
   return (
     <div className="p-5 flex flex-col items-center">
       {loading && <Loader />}
@@ -24,29 +24,27 @@ const Jewelleries = () => {
         <ErrorMessage />
       ) : (
         <div className="flex flex-col items-center w-full">
-
           <main className="mt-3 grid grid-cols-3 gap-3 max-md:grid-cols-2 max-sm:grid-cols-1">
-            {(data as productProps[] || []).map((item) => (
-              <article
-                key={item.id}
-                className="bg-white flex flex-col p-4 rounded cursor-pointer overflow-hidden group"
-              >
-                <div className="flex justify-center h-40 overflow-hidden">
-                  <img
-                    className="h-full object-cover group-hover:scale-125 transition-all duration-500"
-                    src={item.image}
-                    alt={item.title}
-                  />
-                </div>
-                <h3 className="mt-2 text-primary-dark-blue font-medium leading-tight line-clamp-1">
-                  {item.title}
-                </h3>
+            {((data as productProps[]) || []).map((item) => (
+              <Link to={`/shop/${item.id}`} key={item.id}>
+                <article className="bg-white flex flex-col p-4 rounded cursor-pointer overflow-hidden group">
+                  <div className="flex justify-center h-40 overflow-hidden">
+                    <img
+                      className="h-full object-cover group-hover:scale-125 transition-all duration-500"
+                      src={item.image}
+                      alt={item.title}
+                    />
+                  </div>
+                  <h3 className="mt-2 text-primary-dark-blue font-medium leading-tight line-clamp-1">
+                    {item.title}
+                  </h3>
 
-                <div className="flex flex-col mt-auto gap-y-1">
-                  <span className="text-lg font-bold">{`$${item.price}`}</span>
-                  <AddToCartBtn />
-                </div>
-              </article>
+                  <div className="flex flex-col mt-auto gap-y-1">
+                    <span className="text-lg font-bold">{`$${item.price}`}</span>
+                    <AddToCartBtn />
+                  </div>
+                </article>
+              </Link>
             ))}
           </main>
         </div>
