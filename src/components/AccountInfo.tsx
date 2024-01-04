b0940dd314ba4293d63be3a978d3c6defbe18401
Tony Km <kimaxhanthony9@gmail.com>
2024-01-04 15:40:21 +0300

    Edit: AccountInfo.tsx

--- src/components/AccountInfo.tsx
+++ src/components/AccountInfo.tsx
@@ -21,13 +21,15 @@ const AccountInfo = () => {
       </button>
 
       {/* Favorites */}
-      <button className="max-md:hidden">
+      <button className="max-md:hidden relative">
         <MdFavoriteBorder className="w-6 h-6 fill-primary-darkblue"/>
+        <small className="text-white text-xs font-medium w-5 h-5 flex justify-center items-center rounded-full absolute -top-1.5 -right-2.5 bg-primary-orange">0</small>
       </button>
 
+      {/* Carts' */}
       <button className="flex gap-x-1 relative">
         <FaCartArrowDown className="w-6 h-6 fill-secondary-dark-blue"/>
-        <small className="text-white text-xs font-medium w-5 h-5 flex justify-center items-center rounded-full absolute -top-2 left-3 bg-primary-orange">0</small>
+        <small className="text-white text-xs font-medium w-5 h-5 flex justify-center items-center rounded-full absolute -top-1.5 -right-2.5 bg-primary-orange">0</small>
       </button>
 
       {/* Btn to show sm screens navbar */}
